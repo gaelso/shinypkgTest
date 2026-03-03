e1dc8c60c733f5ab55c854e93d1decdc4dfd0e9f
Gael Sola <gaelsola@hotmail.fr>
2026-03-03 17:09:48 +0100

    barebone app done

--- R/mod_home_UI.R
+++ R/mod_home_UI.R
@@ -12,54 +12,59 @@ mod_home_UI <- function(id, i18n){
   ## UI Elements ######
   ##
 
-  ## Hook
-  # hook_text <- i18n$t("APPNAME does this APPNAME does that")
-  #
-  # hook_img
+  ## + Hero section =======
+  ## image first on mobile
+  hero_img <- div(
+    class = "col-md-6 order-1 order-md-2 text-center",
+    tags$img(
+      src = "assets/landing-img.jpg",
+      class = "img-fluid hero-image shadow-sm"
+    )
+  )
 
-  footer <- div(
-    class = "container footer text-center",
-    tags$small(
-      "© 2026 MyApp · Developed by ",
-      tags$strong("Your Name"),
-      " · With the support of XYZ Institute"
+  hero_txt <- div(
+    class = "col-md-6 order-2 order-md-1",
+    h1(class = "hero-title", "A Smarter Way to Explore Data"),
+    p(
+      class = "hero-subtitle mt-3",
+      "Transform complex data into clear insights with a beautiful, intuitive interface."
+    ),
+    br(),
+    ## ACTION BUTTON
+    actionButton(
+      inputId = ns("to_tool"),
+      label = "Get Started",
+      class = "btn btn-primary btn-lg px-4"
     )
   )
 
 
-
+  ## + Features section =======
   card1 <- card(
-    card_header("Welcome"),
-    p("
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula
-      imperdiet finibus. Sed urna sem, molestie at sodales non, viverra vitae
-      mauris.
-      "),
+    class = "feature-card shadow-sm",
+    card_body(
+      h4("Fast & Interactive"),
+      p("Real-time data updates and dynamic visualizations.")
+    )
   )
 
   card2 <- card(
-    card_header(tags$code("arena-helpers")),
-    p("text"),
-    tags$a(
-      href = "https://openforis.github.io/arena-helpers/",
-      alt = "",
-      "More on OF Arena",
-      bsicons::bs_icon("box-arrow-up-right", class = "text-primary"),
-      .noWS = "before-end"
+    class = "feature-card shadow-sm",
+    card_body(
+      h4("Modern Design"),
+      p("Clean layout optimized for desktop and mobile.")
     )
   )
 
   card3 <- card(
-    card_header("{mocaredd}"),
-    p("text"),
-    p(
-      "More on {mocaredd} in:", HTML("&nbsp;"),
-      actionButton(inputId = ns("to_about"), label = "About")
-      )
+    class = "feature-card shadow-sm",
+    card_body(
+      h4("Powerful Analytics"),
+      p("Built with robust R infrastructure and scalable logic."),
+      p("For more information, go to:", HTML("&nbsp;"),
+        actionButton(inputId = ns("to_about"), label = "About")),
+    )
   )
-
-  cards <- list(card1, card2, card3)
-
 
 
   ##
@@ -71,97 +76,24 @@ mod_home_UI <- function(id, i18n){
     # HERO SECTION
     div(
       class = "container hero-section",
-
       div(
         class = "row align-items-center",
-
-        # IMAGE (mobile first)
-        div(
-          class = "col-md-6 order-1 order-md-2 text-center",
-          tags$img(
-            src = "assets/landing-img.jpg",
-            class = "img-fluid hero-image shadow-sm"
-          )
-        ),
-
-        # TEXT
-        div(
-          class = "col-md-6 order-2 order-md-1",
-
-          h1(class = "hero-title", "A Smarter Way to Explore Data"),
-
-          p(
-            class = "hero-subtitle mt-3",
-            "Transform complex data into clear insights with a beautiful, intuitive interface."
-          ),
-
-          br(),
-
-          actionButton(
-            "start",
-            "Get Started",
-            class = "btn btn-primary btn-lg px-4"
-          )
-        )
+        hero_img, hero_txt
       )
     ),
 
-    # FEATURES SECTION
+    # FEAT. SECTION
     div(
       class = "container section-padding",
-
       div(
         class = "row g-4",
-
-        div(
-          class = "col-md-4",
-          card(
-            class = "feature-card shadow-sm",
-            card_body(
-              h4("Fast & Interactive"),
-              p("Real-time data updates and dynamic visualizations.")
-            )
-          )
-        ),
-
-        div(
-          class = "col-md-4",
-          card(
-            class = "feature-card shadow-sm",
-            card_body(
-              h4("Modern Design"),
-              p("Clean layout optimized for desktop and mobile.")
-            )
-          )
-        ),
-
-        div(
-          class = "col-md-4",
-          card(
-            class = "feature-card shadow-sm",
-            card_body(
-              h4("Powerful Analytics"),
-              p("Built with robust R infrastructure and scalable logic.")
-            )
-          )
-        )
+        div(class = "col-md-4", card1),
+        div(class = "col-md-4", card2),
+        div(class = "col-md-4", card3)
       )
     ),
 
     br(),
-
-    h4(
-      icon("arrow-right"), "To start exploring, continue to the:", HTML("&nbsp;"),
-      actionButton(inputId = ns("to_tool"), label = "Tool")
-    ),
-
-    br(),
-
-    div(
-      "{mocaredd}  current version: v1.0.", br(),
-      "Development supported by FAO and the Aim4Forest Programme",
-      style = "font-style: italic;"
-    ),
 
   ) ## END tagList
 
--- tuto-shinypkg.R
+++ tuto-shinypkg.R
@@ -1,134 +1,134 @@
-## Comment before load_all() or install
-
-library(devtools)
-library(usethis)
-library(roxygen2)
-library(here)
-library(tidyverse)
-
-##
-## Initial Setup #######
-##
-
-## Create package
-## use "." to add pkg files to an existing project.
-## Requires overriding the .Rproj file
-usethis::create_package(".")
-
-## Add dependencies, i.e. all the packages that are needed to run the package's functions. Here is a non exhaustive list
-## + Shiny
-usethis::use_package("bslib")
-usethis::use_package("bsicons")
-usethis::use_package("shiny")
-usethis::use_package("shinyjs")
-usethis::use_package("shinyWidgets")
-usethis::use_package("shinyFiles")
-usethis::use_package("shiny.i18n")
-
-## + Tidyverse (Better to call packages individually)
-usethis::use_package("magrittr")
-usethis::use_package("readr")
-usethis::use_package("tidyselect")
-usethis::use_package("dplyr")
-usethis::use_package("ggplot2")
-usethis::use_package("tibble")
-usethis::use_package("forcats")
-usethis::use_package("purrr")
-usethis::use_package("stringr")
-usethis::use_package("rlang")
-# usethis::use_package("tidyverse", type = "depends")
-
-## + Geospatial
-usethis::use_package("sf")
-usethis::use_package("terra")
-usethis::use_package("units")
-
-## Add license
-usethis::use_mit_license()
-
-## Add github actions
-# usethis::use_github_action_check_standard()
-usethis::use_github_action("check-standard")
-
-## Add pkgdown for automatic onkline documentation of the package and
-## hosting tutos on GH pages
-usethis::use_pkgdown()
-usethis::use_github_action("pkgdown")
-
-##
-## Add R and inst/assets folders from the template ######
-##
-
-## Copy/paste manually or with this command
-dl_dir <- "R"
-gh_paths <- gh::gh(
-  "/repos/{owner}/{repo}/contents/{path}",
-  owner = "openforis", repo = "shinypkg-template", path = dl_dir
-  )
-
-if (length(list.files(dl_path)) == 0) {
-  dir.create(dl_dir)
-  purrr::walk(seq_along(gh_paths), function(x){
-    download.file(
-      url = gh_paths[[x]]$download_url,
-      destfile = gh_paths[[x]]$path
-    )
-  })
-}
-
-
-dl_dir <- "inst/assets"
-gh_paths <- gh::gh(
-  "/repos/{owner}/{repo}/contents/{path}",
-  owner = "openforis", repo = "shinypkg-template", path = dl_dir
-  )
-
-if (length(list.files(dl_dir)) == 0) {
-  dir.create("inst")
-  dir.create(dl_dir)
-  purrr::walk(seq_along(gh_paths), function(x){
-    download.file(
-      url = gh_paths[[x]]$download_url,
-      destfile = gh_paths[[x]]$path
-    )
-  })
-}
-
-##
-## Manually ######
-##
-
-## Manually edit the following files to put the correct package name
-## R/zzz.R -> system.file("assets", package = "PKGNAME")
-## shiny_run_APPNAME.R -> system.file("assets/translations.json", package = "PKGNAME")
-
-## Once system.file updated to point to the correct package, the package needs to
-## be installed to make it's assets available to the app
-## !!! Repeat installation every time a new asset is added.
-# devtools::install()
-
-
-
-##
-## Run often ######
-##
-
-## Run often, as soon as you have a new function created or updated
-devtools::load_all()
-shiny_run_APPNAME()
-
-## Update function documentation
-devtools::document()
-
-## Runs checks to find errors in the code that prevent functions
-## to work or notes that would make the package refused by CRAN.
-## Useful for debugging
-devtools::check()
-
-## Install the package from local files
-# devtools::install()
-
-## Install from Github
-# remotes::install_github("GHACCOUNT/PKGNAME")
-
-
+# ## Comment before load_all() or install
+#
+# library(devtools)
+# library(usethis)
+# library(roxygen2)
+# library(here)
+# library(tidyverse)
+#
+# ##
+# ## Initial Setup #######
+# ##
+#
+# ## Create package
+# ## use "." to add pkg files to an existing project.
+# ## Requires overriding the .Rproj file
+# usethis::create_package(".")
+#
+# ## Add dependencies, i.e. all the packages that are needed to run the package's functions. Here is a non exhaustive list
+# ## + Shiny
+# usethis::use_package("bslib")
+# usethis::use_package("bsicons")
+# usethis::use_package("shiny")
+# usethis::use_package("shinyjs")
+# usethis::use_package("shinyWidgets")
+# usethis::use_package("shinyFiles")
+# usethis::use_package("shiny.i18n")
+#
+# ## + Tidyverse (Better to call packages individually)
+# usethis::use_package("magrittr")
+# usethis::use_package("readr")
+# usethis::use_package("tidyselect")
+# usethis::use_package("dplyr")
+# usethis::use_package("ggplot2")
+# usethis::use_package("tibble")
+# usethis::use_package("forcats")
+# usethis::use_package("purrr")
+# usethis::use_package("stringr")
+# usethis::use_package("rlang")
+# # usethis::use_package("tidyverse", type = "depends")
+#
+# ## + Geospatial
+# usethis::use_package("sf")
+# usethis::use_package("terra")
+# usethis::use_package("units")
+#
+# ## Add license
+# usethis::use_mit_license()
+#
+# ## Add github actions
+# # usethis::use_github_action_check_standard()
+# usethis::use_github_action("check-standard")
+#
+# ## Add pkgdown for automatic onkline documentation of the package and
+# ## hosting tutos on GH pages
+# usethis::use_pkgdown()
+# usethis::use_github_action("pkgdown")
+#
+# ##
+# ## Add R and inst/assets folders from the template ######
+# ##
+#
+# ## Copy/paste manually or with this command
+# dl_dir <- "R"
+# gh_paths <- gh::gh(
+#   "/repos/{owner}/{repo}/contents/{path}",
+#   owner = "openforis", repo = "shinypkg-template", path = dl_dir
+#   )
+#
+# if (length(list.files(dl_path)) == 0) {
+#   dir.create(dl_dir)
+#   purrr::walk(seq_along(gh_paths), function(x){
+#     download.file(
+#       url = gh_paths[[x]]$download_url,
+#       destfile = gh_paths[[x]]$path
+#     )
+#   })
+# }
+#
+#
+# dl_dir <- "inst/assets"
+# gh_paths <- gh::gh(
+#   "/repos/{owner}/{repo}/contents/{path}",
+#   owner = "openforis", repo = "shinypkg-template", path = dl_dir
+#   )
+#
+# if (length(list.files(dl_dir)) == 0) {
+#   dir.create("inst")
+#   dir.create(dl_dir)
+#   purrr::walk(seq_along(gh_paths), function(x){
+#     download.file(
+#       url = gh_paths[[x]]$download_url,
+#       destfile = gh_paths[[x]]$path
+#     )
+#   })
+# }
+#
+# ##
+# ## Manually ######
+# ##
+#
+# ## Manually edit the following files to put the correct package name
+# ## R/zzz.R -> system.file("assets", package = "PKGNAME")
+# ## shiny_run_APPNAME.R -> system.file("assets/translations.json", package = "PKGNAME")
+#
+# ## Once system.file updated to point to the correct package, the package needs to
+# ## be installed to make it's assets available to the app
+# ## !!! Repeat installation every time a new asset is added.
+# # devtools::install()
+#
+#
+#
+# ##
+# ## Run often ######
+# ##
+#
+# ## Run often, as soon as you have a new function created or updated
+# devtools::load_all()
+# shiny_run_APPNAME()
+#
+# ## Update function documentation
+# devtools::document()
+#
+# ## Runs checks to find errors in the code that prevent functions
+# ## to work or notes that would make the package refused by CRAN.
+# ## Useful for debugging
+# devtools::check()
+#
+# ## Install the package from local files
+# # devtools::install()
+#
+# ## Install from Github
+# # remotes::install_github("GHACCOUNT/PKGNAME")
+#
+#
